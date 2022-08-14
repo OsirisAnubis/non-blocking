556a0b0bffb40475e62062bc3c7588143b632b3c
OsirisAnubis <olegshulgakalendar@gmail.com>
2022-08-14 15:01:03 +0300

    each non bloking

---
+++ each-non-block.js
@@ -0,0 +1,13 @@
+const each = (arr, fn) => {
+  const len = arr.length;
+  const iter = i => {
+    setTimeout(() => {
+      fn(arr[i]);
+      if (i < len - 1) iter(++i);
+    });
+  };
+  iter(0);
+};
+
+const a = [1, 2, 3, 4, 5, 6];
+each(a, (elem) => console.log(elem));
